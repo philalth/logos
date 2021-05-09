bdfbad9cda55871f9f9a1896be4de3971e422d65
Philipp Althaus <p.althaus98@yahoo.de>
2021-05-09 14:41:30 +0200

    Improved documentation for dataset loading.

--- logos/datasets/datasets.py
+++ logos/datasets/datasets.py
@@ -1,3 +1,8 @@
+"""
+This module contains methods to load commonly used datasets in machine learning
+with predefined parameters.
+"""
+
 import numpy
 import pandas
 from sklearn.datasets import make_circles, make_moons, make_blobs
@@ -5,7 +10,7 @@ from sklearn.datasets import make_circles, make_moons, make_blobs
 
 def load_two_circles():
     """Loads the two circles dataset, a simple toy dataset to visualize
-    logos and classification algorithms.
+    clustering and classification algorithms.
 
     Returns
     -------
@@ -13,53 +18,55 @@ def load_two_circles():
         The generated samples.
 
     y : ndarray of shape (1500,)
-        The integer labels (0 or 1) for class membership of each sample.
+        The integer labels for class membership of each sample.
     """
     return make_circles(n_samples=1500, factor=.5, noise=.05, shuffle=False)
 
 
 def load_two_moons():
     """Loads the two moons dataset, a simple toy dataset to visualize
-    logos and classification algorithms.
+    clustering and classification algorithms.
 
     Returns
     -------
-    X : ndarray of shape (1500, 2)
+    datapoints : ndarray of shape (1500, 2)
         The generated samples.
 
-    y : ndarray of shape (1500,)
-        The integer labels (0 or 1) for class membership of each sample.
+    labels : ndarray of shape (1500,)
+        The integer labels for class membership of each sample.
     """
     return make_moons(n_samples=1500, noise=.05, shuffle=False)
 
 
 def load_anisotropic_blobs():
-    """Generate anisotropic Gaussian blobs for logos.
+    """Generate anisotropic Gaussian blobs for clustering.
 
     Returns
     -------
-    X : ndarray of shape (1500, 2)
+    datapoints : ndarray of shape (1500, 2)
         The generated samples.
 
-    y : ndarray of shape (1500,)
+    labels : ndarray of shape (1500,)
         The integer labels for cluster membership of each sample.
     """
-    datapoints, labels = make_blobs(n_samples=1500, random_state=170,
-                                    shuffle=False)
+    datapoints, labels, _ = make_blobs(n_samples=1500, random_state=170,
+                                       shuffle=False)
+
     transformation = [[0.6, -0.6], [-0.4, 0.8]]
     datapoints = numpy.dot(datapoints, transformation)
+
     return datapoints, labels
 
 
 def load_varied_variances_blobs():
-    """Generate isotropic Gaussian blobs with varied variances for logos.
+    """Generate isotropic Gaussian blobs with varied variances for clustering.
 
     Returns
     -------
-    X : ndarray of shape (1500, 2)
+    datapoints : ndarray of shape (1500, 2)
         The generated samples.
 
-    y : ndarray of shape (1500,)
+    labels : ndarray of shape (1500,)
         The integer labels for cluster membership of each sample.
     """
     return make_blobs(n_samples=1500, cluster_std=[1.0, 2.5, 0.5],
@@ -74,20 +81,25 @@ def load_pulsar():
     Classes                          6
     Samples per class            ~1000
     Samples total                 8000
-    Dimensionality                   2
+    Dimensionality                   8
     Features            real, positive
     =================   ==============
 
-    Source: .https://archive.ics.uci.edu/ml/datasets/HTRU2.
+    Source: https://archive.ics.uci.edu/ml/datasets/HTRU2
 
     Returns
     -------
-    data : ndarray
-        Pulsar data set.
+    datapoints : ndarray of shape (1500, 2)
+        The generated samples.
+
+    labels : ndarray of shape (1500,)
+        The integer labels for cluster membership of each sample.
     """
     data = pandas.read_csv('datasets/pulsar.csv').to_numpy()
+
     datapoints = numpy.array(data[:, 0: 8], dtype=float)
     labels = numpy.array(data[:, 8], dtype=int)
+
     return datapoints, labels
 
 
@@ -104,23 +116,29 @@ def load_mouse():
     Features            real, positive
     =================   ==============
 
-    Source: https://github.com/elki-project/elki/blob/master/data/synthetic/Vorlesung/mouse.csv.
+    Source: https://github.com/elki-project/elki/blob/master/data/synthetic/Vorlesung/mouse.csv
 
     Returns
     -------
-    data : ndarray
-        Mouse data set.
+    datapoints : ndarray of shape (500, 2)
+        The mouse dataset.
+
+    labels : ndarray of shape (500,)
+        The integer labels for cluster membership of each sample.
     """
     data = pandas.read_csv('datasets/mouse.csv').to_numpy()
+
     datapoints = numpy.array(data[:, 0: 2], dtype=float)
     labels = numpy.array(data[:, 2], dtype=int)
+
     return datapoints, labels
 
 
 def load_gaussian_clusters():
     """
     Load and return synthetic Gaussian clusters with different
-    degree of cluster overlap.
+    degree of cluster overlap. Does not come with ground-truth
+    labels.
 
     =================   ==============
     Classes                         15
@@ -130,20 +148,18 @@ def load_gaussian_clusters():
     Features            real, positive
     =================   ==============
 
-    Source: http://cs.joensuu.fi/sipu/datasets/.
+    Source: http://cs.joensuu.fi/sipu/datasets/s1.txt
 
     Returns
     -------
-    data : Bunch
-        Dictionary-like object, the interesting attributes are.
+    datapoints : ndarray of shape (5000, 2)
+        The generated samples.
     """
-    f = open('datasets/s1.txt', 'r')
+    with open('datasets/s1.txt', 'r') as data_file:
+        data = []
+        for line in data_file.readlines():
+            data.append(line.replace('\n', '').split('    ')[1:])
 
-    data = []
-    for line in f.readlines():
-        data.append(line.replace('\n', '').split('    ')[1:])
-
-    f.close()
     return numpy.array(data, dtype=float)
 
 
@@ -159,28 +175,31 @@ def load_spirals():
     Features            real, positive
     =================   ==============
 
-    Source: http://cs.joensuu.fi/sipu/datasets/.
+    Source: http://cs.joensuu.fi/sipu/datasets/spiral.txt
 
     Returns
     -------
-    data : Bunch
-        Dictionary-like object,
+    datapoints : ndarray of shape (312, 2)
+        The spiral dataset.
+
+    labels : ndarray of shape (312,)
+        The integer labels for cluster membership of each sample.
     """
     data = []
     labels = []
 
-    f = open('datasets/spiral.txt', 'r')
-    for line in f.readlines():
-        data.append(line.replace('\n', '').split('\t')[:2])
-        labels.append(line.replace('\n', '').split('\t')[2])
-    f.close()
+    with open('datasets/spiral.txt', 'r') as data_file:
+        for line in data_file.readlines():
+            data.append(line.replace('\n', '').split('\t')[:2])
+            labels.append(line.replace('\n', '').split('\t')[2])
 
     return numpy.array(data, dtype=float), numpy.array(labels, dtype=int)
 
 
 def load_t48k():
     """
-    Load and return the T4-8K data set.
+    Load and return the T4-8K data set. Does not come with ground-truth
+    labels.
 
     =================   ==============
     Classes                          6
@@ -190,18 +209,17 @@ def load_t48k():
     Features            real, positive
     =================   ==============
 
-    Source: http://cs.joensuu.fi/sipu/datasets/.
+    Source: http://cs.joensuu.fi/sipu/datasets/t4.8k.txt
 
     Returns
     -------
-    data : ndarray
-        T4-8K data set.
+    datapoints : ndarray of shape (8000, 2)
+        The T4-8K dataset.
     """
     data = []
 
-    f = open('datasets/t4.8k.txt', 'r')
-    for line in f.readlines():
-        data.append(line.replace('\n', '').split(' '))
-    f.close()
+    with open('datasets/t4.8k.txt', 'r') as data_file:
+        for line in data_file.readlines():
+            data.append(line.replace('\n', '').split(' '))
 
     return numpy.array(data, dtype=float)
